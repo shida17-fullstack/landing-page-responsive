cdfe9846126afb1a4be61c5a4b9ad95295b322d9
shida17-fullstack <abogadanbutera@gmail.com>
2023-12-12 20:53:36 -0300

    Actualizar enlaces y scripts en src/index.html y src/js/cargarParciales.js para la rama cambios-header-instructor

--- src/js/cargarParciales.js
+++ src/js/cargarParciales.js
@@ -1,7 +1,45 @@
-document.addEventListener("DOMContentLoaded", function () {
+/* document.addEventListener("DOMContentLoaded", function () {
     cargarParciales("header", "/src/parciales/header.html");
     cargarParciales("beneficios", "/src/parciales/beneficios.html");
     cargarParciales("instructor", "/src/parciales/instructor.html");
+    cargarParciales("temario", "/src/parciales/temario.html");
+    cargarParciales("testimonios", "/src/parciales/testimonios.html");
+    cargarParciales("footer", "/src/parciales/footer.html");
+
+    // Nuevos parciales Rama cambios-header-instructor
+    cargarParciales("header_cambios", "/src/parciales/header_cambios.html");
+    cargarParciales("instructor_cambios", "/src/parciales/instructor_cambios.html");
+});
+
+function cargarParciales(seccionId, url) {
+    var elemento = document.getElementById(seccionId);
+
+    // Utilizar fetch para obtener el contenido del archivo
+    fetch(url)
+        .then(response => response.text())
+        .then(html => {
+            // Insertar el contenido en el elemento
+            elemento.innerHTML = html;
+        })
+        .catch(error => console.error(`Error loading partial ${seccionId}: ${error}`));
+}
+ */
+
+document.addEventListener("DOMContentLoaded", function () {
+    var currentBranch = "cambios-header-instructor"; // Reemplazar con la lógica adecuada
+
+    if (currentBranch === "cambios-header-instructor") {
+        // Lógica para la rama cambios-header-instructor
+        cargarParciales("header_cambios", "/src/parciales/header_cambios.html");
+        cargarParciales("instructor_cambios", "/src/parciales/instructor_cambios.html");
+    } else {
+        // Lógica para la rama principal
+        cargarParciales("header", "/src/parciales/header.html");
+        cargarParciales("instructor", "/src/parciales/instructor.html");
+    }
+
+    // Estos parciales se cargan en ambas ramas
+    cargarParciales("beneficios", "/src/parciales/beneficios.html");
     cargarParciales("temario", "/src/parciales/temario.html");
     cargarParciales("testimonios", "/src/parciales/testimonios.html");
     cargarParciales("footer", "/src/parciales/footer.html");
---
+++ src/js/cssParciales.js
@@ -0,0 +1,27 @@
+// Obtener la rama actual desde la URL o configuración de tu sistema
+var currentBranch = "main"; // Reemplazar con la lógica adecuada
+
+// Definir un objeto que mapee las rutas de los estilos CSS para cada rama
+var cssPaths = {
+    "main": "/src/css/header.css",
+    "cambios-header-instructor": {
+        "main": "/src/css-cambios/header_cambios.css",
+        "instructor": "/src/css-cambios/instructor_cambios.css"
+    }
+};
+
+// Función para cargar un estilo CSS específico
+function cargarEstilo(ruta) {
+    var styleSheet = document.createElement("link");
+    styleSheet.rel = "stylesheet";
+    styleSheet.href = ruta;
+    document.head.appendChild(styleSheet);
+}
+
+// En función de la rama actual, cargar los estilos correspondientes
+if (currentBranch === "cambios-header-instructor") {
+    cargarEstilo(cssPaths[currentBranch]["main"]);
+    cargarEstilo(cssPaths[currentBranch]["instructor"]);
+} else {
+    cargarEstilo(cssPaths[currentBranch]);
+}
